b7fcc2c6ac796a7f9127ac8f0720591ae60489fe
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DropDownButtonExample/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace DropDownButtonExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dropDownButton1 = new DevExpress.XtraEditors.DropDownButton();
+            this.popupMenu1 = new DevExpress.XtraBars.PopupMenu(this.components);
+            this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+            this.barButtonItem2 = new DevExpress.XtraBars.BarButtonItem();
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.imageCollection1 = new DevExpress.Utils.ImageCollection(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.popupMenu1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCollection1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dropDownButton1
+            // 
+            this.dropDownButton1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dropDownButton1.DropDownControl = this.popupMenu1;
+            this.dropDownButton1.ImageOptions.Image = global::DropDownButtonExample.Properties.Resources.About;
+            this.dropDownButton1.Location = new System.Drawing.Point(0, 0);
+            this.dropDownButton1.Name = "dropDownButton1";
+            this.dropDownButton1.Size = new System.Drawing.Size(371, 44);
+            this.dropDownButton1.TabIndex = 0;
+            this.dropDownButton1.Text = "dropDownButton1";
+            // 
+            // popupMenu1
+            // 
+            this.popupMenu1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem1),
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem2)});
+            this.popupMenu1.Manager = this.barManager1;
+            this.popupMenu1.Name = "popupMenu1";
+            // 
+            // barButtonItem1
+            // 
+            this.barButtonItem1.Caption = "barButtonItem1";
+            this.barButtonItem1.Id = 0;
+            this.barButtonItem1.ImageOptions.ImageIndex = 0;
+            this.barButtonItem1.Name = "barButtonItem1";
+            // 
+            // barButtonItem2
+            // 
+            this.barButtonItem2.Caption = "barButtonItem2";
+            this.barButtonItem2.Id = 1;
+            this.barButtonItem2.ImageOptions.ImageIndex = 1;
+            this.barButtonItem2.Name = "barButtonItem2";
+            // 
+            // barManager1
+            // 
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Images = this.imageCollection1;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.barButtonItem1,
+            this.barButtonItem2});
+            this.barManager1.MaxItemId = 2;
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.barManager1;
+            this.barDockControlTop.Size = new System.Drawing.Size(371, 0);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 296);
+            this.barDockControlBottom.Manager = this.barManager1;
+            this.barDockControlBottom.Size = new System.Drawing.Size(371, 0);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlLeft.Manager = this.barManager1;
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 296);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(371, 0);
+            this.barDockControlRight.Manager = this.barManager1;
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 296);
+            // 
+            // imageCollection1
+            // 
+            this.imageCollection1.ImageStream = ((DevExpress.Utils.ImageCollectionStreamer)(resources.GetObject("imageCollection1.ImageStream")));
+            this.imageCollection1.TransparentColor = System.Drawing.Color.Fuchsia;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 296);
+            this.Controls.Add(this.dropDownButton1);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.popupMenu1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageCollection1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.DropDownButton dropDownButton1;
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraBars.PopupMenu popupMenu1;
+        private DevExpress.Utils.ImageCollection imageCollection1;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem2;
+    }
+}
+
---
+++ VB/DropDownButtonExample/Form1.Designer.vb
@@ -0,0 +1,159 @@
+﻿Namespace DropDownButtonExample
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+            Me.dropDownButton1 = New DevExpress.XtraEditors.DropDownButton()
+            Me.popupMenu1 = New DevExpress.XtraBars.PopupMenu(Me.components)
+            Me.barButtonItem1 = New DevExpress.XtraBars.BarButtonItem()
+            Me.barButtonItem2 = New DevExpress.XtraBars.BarButtonItem()
+            Me.barManager1 = New DevExpress.XtraBars.BarManager(Me.components)
+            Me.barDockControlTop = New DevExpress.XtraBars.BarDockControl()
+            Me.barDockControlBottom = New DevExpress.XtraBars.BarDockControl()
+            Me.barDockControlLeft = New DevExpress.XtraBars.BarDockControl()
+            Me.barDockControlRight = New DevExpress.XtraBars.BarDockControl()
+            Me.imageCollection1 = New DevExpress.Utils.ImageCollection(Me.components)
+            CType(Me.popupMenu1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.barManager1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.imageCollection1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' dropDownButton1
+            ' 
+            Me.dropDownButton1.Dock = System.Windows.Forms.DockStyle.Top
+            Me.dropDownButton1.DropDownControl = Me.popupMenu1
+            Me.dropDownButton1.ImageOptions.Image = My.Resources.About
+            Me.dropDownButton1.Location = New System.Drawing.Point(0, 0)
+            Me.dropDownButton1.Name = "dropDownButton1"
+            Me.dropDownButton1.Size = New System.Drawing.Size(371, 44)
+            Me.dropDownButton1.TabIndex = 0
+            Me.dropDownButton1.Text = "dropDownButton1"
+            ' 
+            ' popupMenu1
+            ' 
+            Me.popupMenu1.LinksPersistInfo.AddRange(New DevExpress.XtraBars.LinkPersistInfo() { _
+                New DevExpress.XtraBars.LinkPersistInfo(Me.barButtonItem1), _
+                New DevExpress.XtraBars.LinkPersistInfo(Me.barButtonItem2) _
+            })
+            Me.popupMenu1.Manager = Me.barManager1
+            Me.popupMenu1.Name = "popupMenu1"
+            ' 
+            ' barButtonItem1
+            ' 
+            Me.barButtonItem1.Caption = "barButtonItem1"
+            Me.barButtonItem1.Id = 0
+            Me.barButtonItem1.ImageOptions.ImageIndex = 0
+            Me.barButtonItem1.Name = "barButtonItem1"
+            ' 
+            ' barButtonItem2
+            ' 
+            Me.barButtonItem2.Caption = "barButtonItem2"
+            Me.barButtonItem2.Id = 1
+            Me.barButtonItem2.ImageOptions.ImageIndex = 1
+            Me.barButtonItem2.Name = "barButtonItem2"
+            ' 
+            ' barManager1
+            ' 
+            Me.barManager1.DockControls.Add(Me.barDockControlTop)
+            Me.barManager1.DockControls.Add(Me.barDockControlBottom)
+            Me.barManager1.DockControls.Add(Me.barDockControlLeft)
+            Me.barManager1.DockControls.Add(Me.barDockControlRight)
+            Me.barManager1.Form = Me
+            Me.barManager1.Images = Me.imageCollection1
+            Me.barManager1.Items.AddRange(New DevExpress.XtraBars.BarItem() { Me.barButtonItem1, Me.barButtonItem2})
+            Me.barManager1.MaxItemId = 2
+            ' 
+            ' barDockControlTop
+            ' 
+            Me.barDockControlTop.CausesValidation = False
+            Me.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top
+            Me.barDockControlTop.Location = New System.Drawing.Point(0, 0)
+            Me.barDockControlTop.Manager = Me.barManager1
+            Me.barDockControlTop.Size = New System.Drawing.Size(371, 0)
+            ' 
+            ' barDockControlBottom
+            ' 
+            Me.barDockControlBottom.CausesValidation = False
+            Me.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom
+            Me.barDockControlBottom.Location = New System.Drawing.Point(0, 296)
+            Me.barDockControlBottom.Manager = Me.barManager1
+            Me.barDockControlBottom.Size = New System.Drawing.Size(371, 0)
+            ' 
+            ' barDockControlLeft
+            ' 
+            Me.barDockControlLeft.CausesValidation = False
+            Me.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left
+            Me.barDockControlLeft.Location = New System.Drawing.Point(0, 0)
+            Me.barDockControlLeft.Manager = Me.barManager1
+            Me.barDockControlLeft.Size = New System.Drawing.Size(0, 296)
+            ' 
+            ' barDockControlRight
+            ' 
+            Me.barDockControlRight.CausesValidation = False
+            Me.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right
+            Me.barDockControlRight.Location = New System.Drawing.Point(371, 0)
+            Me.barDockControlRight.Manager = Me.barManager1
+            Me.barDockControlRight.Size = New System.Drawing.Size(0, 296)
+            ' 
+            ' imageCollection1
+            ' 
+            Me.imageCollection1.ImageStream = (CType(resources.GetObject("imageCollection1.ImageStream"), DevExpress.Utils.ImageCollectionStreamer))
+            Me.imageCollection1.TransparentColor = System.Drawing.Color.Fuchsia
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(371, 296)
+            Me.Controls.Add(Me.dropDownButton1)
+            Me.Controls.Add(Me.barDockControlLeft)
+            Me.Controls.Add(Me.barDockControlRight)
+            Me.Controls.Add(Me.barDockControlBottom)
+            Me.Controls.Add(Me.barDockControlTop)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(Me.popupMenu1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.barManager1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.imageCollection1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+            Me.PerformLayout()
+
+        End Sub
+
+        #End Region
+
+        Private dropDownButton1 As DevExpress.XtraEditors.DropDownButton
+        Private barManager1 As DevExpress.XtraBars.BarManager
+        Private barDockControlTop As DevExpress.XtraBars.BarDockControl
+        Private barDockControlBottom As DevExpress.XtraBars.BarDockControl
+        Private barDockControlLeft As DevExpress.XtraBars.BarDockControl
+        Private barDockControlRight As DevExpress.XtraBars.BarDockControl
+        Private popupMenu1 As DevExpress.XtraBars.PopupMenu
+        Private imageCollection1 As DevExpress.Utils.ImageCollection
+        Private barButtonItem1 As DevExpress.XtraBars.BarButtonItem
+        Private barButtonItem2 As DevExpress.XtraBars.BarButtonItem
+    End Class
+End Namespace
+
