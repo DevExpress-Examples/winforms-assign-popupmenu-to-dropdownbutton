be7681f65ceb3692247449ed862174459e6159e7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DropDownButtonExample/Form1.cs
@@ -0,0 +1,52 @@
+﻿using DevExpress.XtraBars;
+using DevExpress.XtraEditors;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DropDownButtonExample {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            InitDropDownButton();
+        }
+
+        private void InitDropDownButton() {
+            //create DropDownButton
+            DropDownButton dropDownButton = CreateDropDownButton("Button created at runtime", "MyButton", DropDownButtonExample.Properties.Resources.About, this.dropDownButton1.Size);
+            dropDownButton.BringToFront();
+
+            //create menu items
+            DevExpress.XtraBars.BarButtonItem item1 = CreateBarButtonItem("Item 1", "item1", 0);
+            DevExpress.XtraBars.BarButtonItem item2 = CreateBarButtonItem("Item 2", "item2", 1);
+
+            //create and assign PopupMenu
+            dropDownButton.DropDownControl = CreatePopupMenu(new BarItem[] { item1, item2 }, this.barManager1, "MyPopupMenu");
+        }
+
+        private BarButtonItem CreateBarButtonItem(string caption, string name, int imageIndex) {
+            DevExpress.XtraBars.BarButtonItem item = new BarButtonItem();
+            item.Caption = caption;
+            item.Id = this.barManager1.GetNewItemId();            
+            item.ImageIndex = imageIndex;
+            item.Name = name;
+            return item;
+        }
+
+        private DropDownButton CreateDropDownButton(string caption, string name, Image image, Size size) {
+            DropDownButton dropDownButton = new DropDownButton() { Dock = DockStyle.Top };
+            dropDownButton.Name = name;
+            dropDownButton.Text = caption;
+            dropDownButton.Size = size;
+            dropDownButton.Image = DropDownButtonExample.Properties.Resources.About;
+            this.Controls.Add(dropDownButton);       
+            return dropDownButton;
+        }
+
+        private PopupMenu CreatePopupMenu(BarItem[] items, BarManager manager, string name) {
+            PopupMenu menu = new PopupMenu(manager);         
+            menu.Name = name;
+            menu.AddItems(items);
+            return menu;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,11 @@
+# How to use the DropDownButton control
+
+
+<p>This example demonstrates how to assign <strong>PopupMenu</strong> to the <strong>DropDownButton</strong> control at design time and how to create <strong>DropDownButton</strong> and assign its <a href="https://documentation.devexpress.com/WindowsForms/DevExpress.XtraEditors.DropDownButton.DropDownControl.property">DropDownControl</a> at runtime (see the Form1.cs file).</p>
+<p>Please note that starting with version 16.2 we slightly changed our API and images should be assigned using the <a href="https://documentation.devexpress.com/WindowsForms/DevExpress.XtraBars.BarButtonItem.ImageOptions.property">ImageOptions</a> property.</p>
+<p><br>The following animation illustrates the process of assigning <strong>PopupMenu</strong> to the <strong>DropDownButton</strong> control at design time:</p>
+<p><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-use-the-dropdownbutton-control-e424/13.1.4+/media/819596a2-eee7-4c6a-be61-eb92547a333d.png"></p>
+
+<br/>
+
+
